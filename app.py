c75a57f9b9d1a76a3cd89f6e2d89cfc73217627b
piyushkanadje <piyushkanadje@gmail.com>
2024-11-10 16:41:11 -0800

    app.py change and removed the config

--- app.py
+++ app.py
@@ -16,21 +16,9 @@ logger = logging.getLogger(__name__)
 def create_app():
     app = Flask(__name__)
 
-    # Load environment-specific configurations
-    app.config['DEBUG'] = os.getenv('FLASK_DEBUG', 'False') == 'True'
-    app.config['ENV'] = os.getenv('FLASK_ENV', 'production')
-    app.config['SESSION_COOKIE_SECURE'] = True
-    app.config['SESSION_COOKIE_HTTPONLY'] = True
-    app.config['PREFERRED_URL_SCHEME'] = 'https'
-
+    
     # Register Blueprints
     app.register_blueprint(receipt_processor_api)
-
-    # Global error handler
-    @app.errorhandler(Exception)
-    def handle_unexpected_error(error):
-        logger.exception("An unexpected error occurred")
-        return jsonify({"error": "An unexpected error occurred"}), 500
 
     return app
 
@@ -40,6 +28,6 @@ if __name__ == '__main__':
     # Run the app with configurable host and port
     app.run(
         host=os.getenv('FLASK_RUN_HOST', '0.0.0.0'),
-        port=int(os.getenv('FLASK_RUN_PORT', '5000')),
+        port=int(os.getenv('FLASK_RUN_PORT', '5001')),
         debug=app.config['DEBUG']
     )
